2f60b6f315262dbf37ecff5042761738397d16ae
Tausif Muzaffar <tausif.muzaffar@salesforce.com>
2017-05-23 09:43:59 -0400

    Demo

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var port = process.env.PORT || 5000;
 var app = express();
- 
+
 app.get('/', function(request, response) {
-    response.sendfile(__dirname + '/public/index.html');
+    response.sendfile(__dirname + '/public/rest.html');
 }).configure(function() {
     app.use('/', express.static(__dirname + '/public/'));
 }).listen(port);
