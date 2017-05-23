ddc2d8014aebc29bc9fd242eea532a7bcab2156b
Tausif Muzaffar <tausif.muzaffar@salesforce.com>
2017-05-23 09:32:30 -0400

    Demo

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
-var express = require('express');
-var app = express();
-const path = require('path');
-
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('*', function (req, res) {
-  const index = path.join(__dirname,'public','index.html');
-  res.sendFile(index);
-});
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-
+var static = require('node-static');
+var file = new static.Server();
+require('http').createServer(function(request, response) {
+  request.addListener('end', function() {
+    file.serve(request, response);
+  }).resume();
+}).listen(process.env.PORT || 3000);
