16ba6f66ae992767b38fbe4c2f51e16d0234bc3b
Tausif Muzaffar <tausif.muzaffar@salesforce.com>
2017-05-23 08:54:13 -0400

    Demo

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
