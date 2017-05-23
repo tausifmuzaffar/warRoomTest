200035245e4fdb05f873ef10092072da8f979324
Tausif Muzaffar <tausif.muzaffar@salesforce.com>
2017-05-23 09:50:55 -0400

    Demo

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.sendFile(__dirname + '/rest.html');
 });
 
 app.listen(app.get('port'), function() {
