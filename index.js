6a70314cfb68726e171d22b272dd1b1f9cd191d5
Tausif Muzaffar <tausif.muzaffar@salesforce.com>
2017-05-23 08:47:14 -0400

    git heroic

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('connected', function (widgetName, widgetLocation, actions, variables) {
-    console.log("Widget Connected: "+widgetName+" "+widgetLocation+" "+actions+" "+variables);
-    io.emit('newWidget', widgetName, widgetLocation, actions, variables);
-    io.emit('log', "Widget Connected: "+widgetName+" "+widgetLocation+" "+actions+" "+variables);
-  });
-  socket.on('log', function (msg) {
-    io.emit('log',msg);
-    console.log("Log: "+ msg);
-  });
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+
